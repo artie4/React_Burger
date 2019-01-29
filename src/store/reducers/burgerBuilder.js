7a1e075898543fd8c05933a7727fb36f2dd97735
arthur <gaartur4@gmail.com>
2019-01-29 22:31:47 +0300

    fixed removing ingr

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -25,11 +25,11 @@ const addIngredient = (state, action) => {
 };
 
 const removeIngredient = (state, action) => {
-    const updatedIng = {[action.ingredientName]: state.ingredients[action.ingredientName] + 1};
+    const updatedIng = {[action.ingredientName]: state.ingredients[action.ingredientName] - 1};
     const updatedIngs = updateObject(state.ingredients, updatedIng);
     const updatedSt = {
         ingredients: updatedIngs,
-        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
     };
     return updateObject(state, updatedSt);
 };
