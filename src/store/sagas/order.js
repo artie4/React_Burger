1264e21c34fb1d6c2ccb7c49dbbbc715b559a8bb
arthur <gaartur4@gmail.com>
2019-02-05 17:21:33 +0300

    removed unused imports

--- src/store/sagas/order.js
+++ src/store/sagas/order.js
@@ -1,7 +1,6 @@
 import { put } from 'redux-saga/effects';
 import axios from "../../axios-orders";
 import * as actions from '../actions';
-import {fetchOrdersFail, fetchOrdersStart, fetchOrdersSuccess} from "../actions/order";
 
 export function* purchaseBurgerSaga(action) {
 
